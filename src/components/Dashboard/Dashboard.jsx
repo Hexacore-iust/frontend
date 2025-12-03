c73bc5844c150289f505a5beb7eb96c2affeb330
parmisvazifeh <parmisvaziferm@gmail.com>
2025-12-03 22:18:58 +0330

    Fix: Fixed component dashboard style and api calls

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,6 +1,32 @@
 import React from "react";
+// import { useEffect } from "react";
+// import { baseUrl } from "../../config";
+// import axios from "axios";
+import { statistics, scheduleToday } from "../../homepage-mockdata";
+import "./Dashboard.styles.scss";
+import CustomAccordion from "../Accordion/CustomAccordion";
 
 const Dashboard = () => {
+  const { schedule } = scheduleToday;
+  // const token = localStorage.getItem("token");
+  // const token =
+  //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTc2NTM3NTEzOSwiaWF0IjoxNzY0NzcwMzM5LCJqdGkiOiI5ODdjZTRlNGVjYWM0YjQ3ODY2OWI4MmVmYTE2NWQzNiIsInVzZXJfaWQiOjF9.82Prhk5Fr_Qzj6OyOxkSyh27YyAd3x_Zh4MudjQV7AU";
+
+  // const getDashboard = () => {
+  //   axios({
+  //     method: "get",
+  //     headers: { Authorization: `Bearer ${token}` },
+  //     url: `${baseUrl}/api/homepage/`,
+  //     responseType: "application/json",
+  //   }).then(function (response) {
+  //     console.log("res", response);
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   getDashboard();
+  // }, []);
+
   return (
     <div className="content-wrapper">
       <div className="text-blocks">
@@ -9,28 +35,55 @@ const Dashboard = () => {
       </div>
       <div className="task-blocks">
         {/* Task, Meeting, Action Blocks */}
-        <div className="block">
+        <div className="task-blocks__statistic-box">
           <h4>قرار های پیش رو</h4>
-          <p>Content goes here</p>
+          <p>{statistics.upcoming_meetings}</p>
         </div>
-        <div className="block">
+        <div className="task-blocks__statistic-box">
           <h4>کار های پیش رو</h4>
-          <p>Content goes here</p>
+          <p>{statistics.upcoming_tasks}</p>
         </div>
-        <div className="block">
-          <h4>به پایان رسیده</h4>
-          <p>Content goes here</p>
+        <div className="task-blocks__statistic-box">
+          <h4>کار های باقی مانده</h4>
+          <p>{statistics.overdue_tasks}</p>
         </div>
       </div>
 
       <div className="today-plan">
-        {/* Today's Plan */}
         <h3>برنامه امروز من:</h3>
-        <ul>
-          <li>Item 1</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-        </ul>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "8px",
+          }}
+        >
+          {schedule.map((item) => {
+            return (
+              <CustomAccordion
+                summary={item.title}
+                detailsChildren={
+                  <div
+                    style={{
+                      display: "flex",
+                      gap: "8px",
+                      flexDirection: "column",
+                      color: "#777",
+                    }}
+                  >
+                    <div>{item.description}</div>
+                    <div>
+                      {item.start_time} {item.end_time}
+                    </div>
+                    <div>{item.time}</div>
+                  </div>
+                }
+                hasAction={false}
+                color={item.category.color}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
