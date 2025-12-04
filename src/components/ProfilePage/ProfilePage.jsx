7c3d5b32709e2aaed574742b6915ab83f150f0c5
parmisvazifeh <parmisvaziferm@gmail.com>
2025-12-04 23:30:48 +0330

    Fix

--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import dayjs from "dayjs";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import CustomDatePicker from "../DatePicker/DatePicker";
