060f9d85d9f89a792fb0f5873bbbfaf05b798367
parmisvazifeh <parmisvaziferm@gmail.com>
2026-01-01 23:04:51 +0330

    Fix: Fixed miniApp

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,28 +1,12 @@
 import React from "react";
-import { useNavigate, useEffect } from "react-router-dom";
+
 import HomeHeader from "../components/HeaderLanding";
 import HeroSection from "../components/landing/landing1";
 import WhySection from "../components/landing/landing2";
 import TeamSection from "../components/landing/landing3";
 import Footer from "../components/Footer";
-import { tokenStorage } from "../api/axios";
 
 const Landing = () => {
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const tg = window.Telegram?.WebApp;
-    const initData = tg?.initData;
-
-    if (initData) {
-      navigate("/telegram-auth", { replace: true });
-      return;
-    }
-    const existingAccess = tokenStorage.getAccess();
-    if (existingAccess) {
-      navigate("/homepage", { replace: true });
-    }
-  }, [navigate]);
   return (
     <>
       <HomeHeader />
@@ -34,4 +18,4 @@ const Landing = () => {
   );
 };
 
-export default Landing;
+export default Landing; 
