18c4c4514e655313e7edc0969d014f7f64f5e68d
parmisvazifeh <parmisvaziferm@gmail.com>
2026-01-05 15:31:38 +0330

    Fix: fixed login into mui and fixed formatting

--- src/theme.js
+++ src/theme.js
@@ -5,6 +5,17 @@ import { faIR as pickersFaIR } from "@mui/x-date-pickers/locales";
 const theme = createTheme(
   {
     direction: "rtl",
+    typography: {
+      fontFamily: [
+        "Vazirmatn",
+        "system-ui",
+        "-apple-system",
+        "BlinkMacSystemFont",
+        "Segoe UI",
+        "Roboto",
+        "sans-serif",
+      ].join(","),
+    },
   },
   coreFaIR,
   pickersFaIR
